bc9341eb37d60cb7510c797893a0f844a3098f33
surnan <forsuresh@yandex.com>
2018-09-11 18:39:51 -0400

    Runs without storyboard

--- Employee-Company-2D_Array-CoreData/Employee-Company-2D_Array-CoreData/ViewController.swift
+++ Employee-Company-2D_Array-CoreData/Employee-Company-2D_Array-CoreData/ViewController.swift
@@ -12,14 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.yellow
+        navigationController?.navigationBar.backgroundColor = .red
+        navigationController?.navigationBar.isTranslucent = false
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
