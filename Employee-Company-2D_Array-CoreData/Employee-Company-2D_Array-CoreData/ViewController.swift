cb76e58c4111b8710066cf2fc61c46c3cfc91c37
surnan <forsuresh@yandex.com>
2018-09-11 08:35:30 -0400

    Project Created

---
+++ Employee-Company-2D_Array-CoreData/Employee-Company-2D_Array-CoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Employee-Company-2D_Array-CoreData
+//
+//  Created by admin on 9/11/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
